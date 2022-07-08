991de0ff3a6cb753878ecb8e9ac604eac5d17f4a
Alexandre Guidoux <alexandre.guidoux@ergosign.de>
2022-07-08 19:23:20 +0200

    ➕ blurhash and histoire

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
+/// <reference types="histoire" />
+
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  histoire: {}
 })
