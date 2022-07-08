70c2d981bffd3a6b4c20dd4433b463fc76726f47
Alexandre Guidoux <alexandre.guidoux@ergosign.de>
2022-07-08 20:19:41 +0200

    🎨(lib/index.ts)

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ import type { Plugin } from 'vue'
 import BlurHashCanvas from './BlurHashCanvas.vue'
 
 const plugin: Plugin = {
-  install(Vue, options) {
+  install(Vue) {
     Vue.component('blur-hash-canvas', BlurHashCanvas)
   }
 }
