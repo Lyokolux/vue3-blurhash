155dbef3c084096299e46ef0e3bb8f8116dbc8a0
Alexandre Guidoux <alexandre.guidoux@ergosign.de>
2022-07-09 17:10:49 +0200

    🔧(package): proper config to build vue components and ts files

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,44 @@
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+const LIBRARY_NAME = 'vue3-blurhash'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      include: 'lib',
+      outputDir: 'dist/types'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: LIBRARY_NAME,
+      formats: ['es', 'umd', 'cjs'],
+      fileName: (format) => `${LIBRARY_NAME}.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
   },
   histoire: {}
 })
